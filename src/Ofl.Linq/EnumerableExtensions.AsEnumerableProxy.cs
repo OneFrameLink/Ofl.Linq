77adbd304d9228c02c22b8fdd8470acfc834bdcd
casperOne <casperOne@caspershouse.com>
2017-05-16 16:11:40 -0400

    Initial code push.

---
+++ src/Ofl.Linq/EnumerableExtensions.AsEnumerableProxy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ofl.Linq
+{
+    public static partial class EnumerableExtensions
+    {
+        public static EnumerableProxy<T> AsEnumerableProxy<T>(this IEnumerable<T> enumerable)
+        {
+            // Validate parameters.
+            if (enumerable == null) throw new ArgumentNullException(nameof(enumerable));
+
+            // Return the new proxy.
+            return new EnumerableProxy<T>(enumerable);
+        }
+    }
+}
---
+++ src/Ofl.Linq/EnumerableProxy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ofl.Linq
+{
+    public class EnumerableProxy<T> : IEnumerable<T>
+    {
+        #region Constructor
+
+        public EnumerableProxy(IEnumerable<T> enumerable)
+        {
+            // Validate parameters.
+            _enumerable = enumerable ?? throw new ArgumentNullException(nameof(enumerable));
+        }
+
+        #endregion
+
+        #region Private, reado-only state.
+
+        private readonly IEnumerable<T> _enumerable;
+
+        #endregion
+
+        #region IEnumerable<T> implementation.
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IEnumerator<T> GetEnumerator() => _enumerable.GetEnumerator();
+
+        #endregion
+    }
+}
